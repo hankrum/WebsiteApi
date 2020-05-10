85d37f808006aae855f1cce525bf0e96b80c4378
Stefan Dantchev <st_dantchev@abv.bg>
2020-05-11 02:51:44 +0300

    add unit of work

---
+++ WebsiteApi/Api.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Api.Data.Model;
+using Api.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IEfRepository<WebSite> WebSites { get; }
+
+        IEfRepository<Category> Categories { get; }
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ WebsiteApi/Api.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using Api.Data.Model;
+using Api.Data.Repository;
+using Api.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MsSqlDbContext context;
+        private IEfRepository<WebSite> webSites;
+        private IEfRepository<Category> categories;
+
+        public UnitOfWork(
+            MsSqlDbContext context, 
+            IEfRepository<WebSite> webSites,
+            IEfRepository<Category> categories
+            )
+        {
+            Validated.NotNull(context, nameof(context));
+            Validated.NotNull(webSites, nameof(webSites));
+            Validated.NotNull(categories, nameof(categories));
+            this.context = context;
+            this.webSites = webSites;
+            this.categories = categories;
+        }
+
+        public IEfRepository<WebSite> WebSites
+        {
+            get
+            {
+                return this.webSites;
+            }
+        }
+
+        public IEfRepository<Category> Categories
+        {
+            get
+            {
+                return this.categories;
+            }
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await context.SaveChangesAsync();
+        }
+    }
+}
