364ff47f7cb57ff476d9c01a18f54fb5361378e3
Stefan Dantchev <st_dantchev@abv.bg>
2020-05-11 00:30:19 +0300

    Data project and models

---
+++ WebsiteApi/Api.Data/Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Api.Data.Model
+{
+    public class Category
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public HashSet<WebSite> WebSites { get; set; }
+}
+}
---
+++ WebsiteApi/Api.Data/Model/WebSite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Api.Data.Model
+{
+    public class WebSite
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Url]
+        public string Url { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        [Required]
+        [Url]
+        public string SnapshotUrl { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string LoginEmail { get; set; }
+
+        [Required]
+        public string LoginPassword { get; set; }
+    }
+}
