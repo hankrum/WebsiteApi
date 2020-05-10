c22770bde85b902a437127675b0c7ba8a5ef5ef8
Stefan Dantchev <st_dantchev@abv.bg>
2020-05-11 02:20:04 +0300

    add validator

---
+++ WebsiteApi/Api.Infrastructure/Validated.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Api.Infrastructure
+{
+    public static class Validated
+    {
+        public static void NotNull(object tested, string message)
+        {
+            if (tested == null)
+            {
+                throw new ArgumentNullException(message);
+            }
+        }
+    }
+}
