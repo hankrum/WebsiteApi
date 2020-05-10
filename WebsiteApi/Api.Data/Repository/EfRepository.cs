519e0de1692670b9c0c528a40062dd26fcb0bbb6
Stefan Dantchev <st_dantchev@abv.bg>
2020-05-11 02:20:33 +0300

    Add repository

---
+++ WebsiteApi/Api.Data/Repository/EfRepository.cs
@@ -0,0 +1,59 @@
+﻿using Api.Infrastructure;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.Repository
+{
+    public class EFRepository<T> : IEfRepository<T> where T : class
+    {
+        private readonly MsSqlDbContext context;
+
+
+        public EFRepository(MsSqlDbContext context)
+        {
+            Validated.NotNull(context, nameof(context));
+            this.context = context;
+        }
+
+        public IQueryable<T> All
+        {
+            get
+            {
+                return this.context.Set<T>().AsQueryable();
+            }
+        }
+
+        public T Add(T entity)
+        {
+            Validated.NotNull(entity, nameof(entity));
+            EntityEntry entry = this.context.Entry(entity);
+
+            return this.context.Set<T>().Add(entity).Entity;
+        }
+
+        public T Delete(T entity)
+        {
+            Validated.NotNull(entity, nameof(entity));
+            var entry = this.context.Entry(entity);
+
+            return this.context.Set<T>().Remove(entity).Entity;
+        }
+
+        public T Update(T entity)
+        {
+            Validated.NotNull(entity, nameof(entity));
+            EntityEntry entry = this.context.Entry(entity);
+
+            return this.context.Set<T>().Update(entity).Entity;
+        }
+
+        public async Task<T> GetById(long id)
+        {
+            return await this.context.Set<T>().FindAsync(id);
+        }
+    }
+}
---
+++ WebsiteApi/Api.Data/Repository/IEfRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.Repository
+{
+    public interface IEfRepository<T> where T : class
+    {
+        IQueryable<T> All { get; }
+
+        T Add(T entity);
+
+        T Delete(T entity);
+
+        T Update(T entity);
+
+        Task<T> GetById(long id);
+    }
+}
