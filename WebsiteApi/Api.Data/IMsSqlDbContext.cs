8db9bafb7dff2fbc44c162d0941a4104aa98f634
Stefan Dantchev <st_dantchev@abv.bg>
2020-05-11 01:26:44 +0300

    Add DbContext

---
+++ WebsiteApi/Api.Data/IMsSqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Api.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Data
+{
+    public interface IMsSqlDbContext : IDisposable
+    {
+        DbSet<WebSite> WebSites { get; set; }
+
+        DbSet<Category> Categories { get; set; }
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        int SaveChanges();
+    }
+}
---
+++ WebsiteApi/Api.Data/MsSqlDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Api.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api.Data
+{
+    public class MsSqlDbContext : DbContext, IMsSqlDbContext
+    {
+        public MsSqlDbContext(DbContextOptions<MsSqlDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebSite> WebSites { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+
+        public new DbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+    }
+}
