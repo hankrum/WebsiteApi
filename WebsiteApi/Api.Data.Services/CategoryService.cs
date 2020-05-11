7276adbce1d3833b674d788a1cd6844bea6a9adc
Stefan Dantchev <st_dantchev@abv.bg>
2020-05-11 03:21:43 +0300

    services project, category service

---
+++ WebsiteApi/Api.Data.Services/CategoryService.cs
@@ -0,0 +1,70 @@
+﻿using Api.Data.Model;
+using Api.Data.UnitOfWork;
+using Api.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Data.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CategoryService(IUnitOfWork unitOfWork)
+        {
+            Validated.NotNull(unitOfWork, nameof(unitOfWork));
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Category>> All()
+        {
+            IEnumerable<Category> categories = await this.unitOfWork.Categories.All.ToListAsync();
+            return categories;
+        }
+
+        public async Task<Category> GetById(long id)
+        {
+            return await this.unitOfWork.Categories.GetById(id);
+        }
+
+        public bool IsEmpty()
+        {
+            return !this.unitOfWork.Categories.All.Any();
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            Validated.NotNull(category, nameof(category));
+
+            Category addedSportEvent = this.unitOfWork.Categories.Add(category);
+
+            await this.unitOfWork.SaveChanges();
+
+            return addedSportEvent;
+        }
+
+        public async Task<Category> Delete(long id)
+        {
+            var model = await this.unitOfWork.Categories.GetById(id);
+            Category deletedSportEvent = this.unitOfWork.Categories.Delete(model);
+
+            await this.unitOfWork.SaveChanges();
+
+            return deletedSportEvent;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            Validated.NotNull(category, nameof(category));
+
+            Category updatedSportEvent = this.unitOfWork.Categories.Update(category);
+
+            await this.unitOfWork.SaveChanges();
+
+            return updatedSportEvent;
+        }
+    }
+}
---
+++ WebsiteApi/Api.Data.Services/ICategoryService.cs
@@ -0,0 +1,23 @@
+﻿using Api.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> All();
+
+        bool IsEmpty();
+
+        Task<Category> GetById(long id);
+
+        Task<Category> Create(Category category);
+
+        Task<Category> Delete(long id);
+
+        Task<Category> Update(Category model);
+    }
+}
