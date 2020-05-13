844b87ea44d6431f9fe87af9b06dc4c0340ab2ee
Stefan Dantchev <st_dantchev@abv.bg>
2020-05-13 03:02:23 +0300

    controller

--- WebsiteApi/Api.Host/Controllers/WebSiteController.cs
+++ WebsiteApi/Api.Host/Controllers/WebSiteController.cs
@@ -24,7 +24,7 @@ namespace Api.Host.Controllers
 
         // GET: api/WebSite
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<IEnumerable<WebSite>>> Get()
         {
             // TODO: pagination and sorting
             var result = await this.websiteService.All();
@@ -37,26 +37,47 @@ namespace Api.Host.Controllers
             return Ok(result);
         }
 
-        //// GET: api/WebSite/5
-        //[HttpGet("{id}", Name = "Get")]
-        //public Task<IActionResult> Get(int id)
-        //{
-        //    return "value";
-        //}
+        // GET: api/WebSite/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<ActionResult<WebSite>> Get(int id)
+        {
+            var website = await this.websiteService.GetById(id);
+
+            if (website == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(website);
+        }
 
         // POST: api/WebSite
         [HttpPost]
-        public async Task<WebSite> Post([FromBody] WebSite webSite)
+        public async Task<ActionResult<WebSite>> Post([FromBody] WebSite webSite)
         {
             var result = await this.websiteService.Create(webSite);
 
-            return result;
+            return CreatedAtAction(
+                nameof(this.Post),
+                new {id = webSite.Id }, 
+                result
+            );
         }
 
         // PUT: api/WebSite/5
         [HttpPut("{id}")]
-        public async Task<WebSite> Put(long id, [FromBody] WebSite webSite)
+        public async Task<ActionResult<WebSite>> Put(long id, [FromBody] WebSite webSite)
         {
+            if (id != webSite.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!await this.websiteService.Exists(id))
+            {
+                return NotFound();
+            }
+
             var result = await this.websiteService.Update(webSite);
 
             return result;
@@ -64,8 +85,16 @@ namespace Api.Host.Controllers
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<WebSite>> Delete(int id)
         {
+            if (!await this.websiteService.Exists(id))
+            {
+                return NotFound();
+            }
+
+            var result = await this.websiteService.Delete(id);
+
+            return result;
         }
     }
 }
